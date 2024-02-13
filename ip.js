9b5f6043e4e57f36031f9ac57ea031608502aad5
Ahmedeii <ahmedabdalnabe77@gmail.com>
2024-02-13 06:28:39 +0200

    first commit

---
+++ ip.js
@@ -0,0 +1,8 @@
+let imgoe=document.getElementById("imgoe")
+let cho =document.getElementById("count")
+function change(change){
+ imgoe.src=change 
+}
+function color(color){
+  cho.style.background =color;  
+}
